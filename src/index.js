7b551149bc2234ecaf5df81b9065042dad6e57ee
Daniel Schischkin | Surplex <daniel.schischkin@surplex.com>
2021-02-04 16:11:35 +0100

    Adjust index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-export { default as default } from './Component.svelte';
+export { default as Header } from "./Header.svelte";
+export { default as Row, RowDefinition } from "./Row.svelte";
+export { default as Table } from "./Table.svelte";
+export { default as Column, ColumnDefinition, EditEvent } from "./Column.svelte";
+export { default as Filter, FilterEvent } from "./Misc/Filter.svelte";
+export { default as Sort, SortEvent } from "./Misc/Sort.svelte";
